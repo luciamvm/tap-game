7620fc6258d8f1b8bd23fce176df5af35bc5e8bf
lucia <74472796+luciamvm@users.noreply.github.com>
2023-05-05 16:20:05 +0100

    simple tap game with countdown and score

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,122 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
+import { useEffect, useRef, useState } from 'react';
+
+// export a ramdom number between min and max
+const ramdomNumber = (min: number, max: number) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+const Home = () => {
+  const gameRef = useRef<HTMLDivElement>(null);
+  const circlesRef = useRef<HTMLDivElement>(null);
+  const startRef = useRef<HTMLDivElement>(null);
+  const counterRef = useRef<HTMLDivElement>(null);
+  const scoreRef = useRef<HTMLDivElement>(null);
+
+  const [score, setScore] = useState(0);
+  const [start, setStart] = useState(false);
+
+  const addElement = () => {
+    const velocity = ramdomNumber(5000, 9000);
+    const size = 70;
+    const positionX = ramdomNumber(100, window.innerWidth - 100);
+
+    const circle = document.createElement('div');
+    circle.style.cssText = `position:absolute; top: -10vh ;width: ${size}px; height: ${size}px; left: ${positionX}px; border-radius: 50% ;transition: transform ${velocity}ms linear; z-index:9;`;
+
+    // set color
+    if (Math.round(Math.random()) < 0.5) {
+      circle.style.background = 'red';
+    } else {
+      circle.style.background = 'green';
+    }
+
+    // add circle on page
+    circlesRef.current!.appendChild(circle);
+
+    // add transition
+    setTimeout(() => {
+      circle.className = 'move';
+    }, ramdomNumber(0, 5000));
+
+    // remove circle when transition ends
+    circle.addEventListener('transitionend', () => {
+      circle.remove();
+    });
+
+    // update score and remove circle
+    circle.addEventListener('click', () => {
+      if (circle.style.background == 'red') {
+        setScore((prevScore) => prevScore - 1);
+      }
+      if (circle.style.background == 'green') {
+        setScore((prevScore) => prevScore + 1);
+      }
+      circle.remove();
+    });
+  };
+
+  useEffect(() => {
+    if (start) {
+      startRef.current!.style.opacity = '0';
+
+      const runGame = setInterval(() => {
+        addElement();
+      }, 500);
+
+      // count down
+      let seconds = 30;
+      const countDown = setInterval(() => {
+        if (seconds > 0) {
+          seconds -= 1;
+          if (seconds < 10) {
+            counterRef.current!.innerText = `00:0${seconds}s`;
+          } else {
+            counterRef.current!.innerText = `00:${seconds}s`;
+          }
+
+          if (seconds === 5) {
+            clearInterval(runGame);
+          }
+        } else {
+          clearInterval(countDown);
+          scoreRef.current!.style.zIndex = '2';
+          scoreRef.current!.style.opacity = '1';
+
+          circlesRef.current?.replaceChildren();
+        }
+      }, 1000);
+    }
+  }, [start]);
+
+  const tryAgain = () => {
+    setStart(false);
+    setScore(0);
+    counterRef.current!.innerText = '00:30s';
+    startRef.current!.style.opacity = '1';
+    scoreRef.current!.style.zIndex = '0';
+    scoreRef.current!.style.opacity = '0';
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="game" ref={gameRef}>
+      <div className="circles" ref={circlesRef}></div>
+      <div className="counter" ref={counterRef}>
+        00:30s
+      </div>
+      <div className="start" ref={startRef} onClick={() => setStart(true)}>
+        Ckick to start
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="score" ref={scoreRef}>
+        {score}
+        <button className="buttonTryAgain" onClick={tryAgain}>
+          Try again
+        </button>
       </div>
+    </div>
+  );
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
